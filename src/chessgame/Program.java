5f50632a96a8e6bb8cc15bc5d9b72ab5d4575535
Lucky9-original <lucianito.araujo@gmail.com>
2019-08-21 22:26:18 +0100

    Terceira Parte

--- src/chessgame/Program.java
+++ src/chessgame/Program.java
@@ -2,13 +2,30 @@ package chessgame;
 
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner tec = new Scanner(System.in);
         
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println("");
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(tec);
+            
+            System.out.println("");
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(tec);
+            
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+        
     }
     
 }
